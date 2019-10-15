682fa5551329de161b134f2af6501de8680c05b6
Kanwal-Itmiaz <kanwalimtiaz94@gmail.com>
2019-10-15 15:30:59 +1100

    Updates

---
+++ precache-manifest.565a4bb76d663281985a16ea5cb35fbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee56d5e8c82075d4c622674cea9daee",
+    "url": "/FaceRecognitionReact/index.html"
+  },
+  {
+    "revision": "44e6612a0116b7ad63ce",
+    "url": "/FaceRecognitionReact/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5a931bc4548107e43f5b",
+    "url": "/FaceRecognitionReact/static/css/main.dbd91eaf.chunk.css"
+  },
+  {
+    "revision": "44e6612a0116b7ad63ce",
+    "url": "/FaceRecognitionReact/static/js/2.23a610cb.chunk.js"
+  },
+  {
+    "revision": "5a931bc4548107e43f5b",
+    "url": "/FaceRecognitionReact/static/js/main.88d30105.chunk.js"
+  },
+  {
+    "revision": "852dd26da20b56b4792d",
+    "url": "/FaceRecognitionReact/static/js/runtime-main.42098f9f.js"
+  }
+]);
