0db0501f686ecd8b39c8673da386ae7a078b21c0
Kanwal-Itmiaz <kanwalimtiaz94@gmail.com>
2019-10-15 15:32:07 +1100

    Building components for the Image Recognition App

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Rank()
+{
+		return(
+
+			<div>
+				<div className ='white f3 tc'>
+					{'Kanwal, your current rank is ...'}
+				</div>
+
+				<div className ='white f1 tc'>
+					{'#5'}
+				</div>
+			</div>
+
+			);
+	
+}
+
+export default Rank;
